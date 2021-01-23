901e98f4b084e0cb6b6860d8bb6a73543f3b9d98
jamilxt <jamilxt@gmail.com>
2021-01-23 14:13:52 +0600

    add: implement Server Streaming API Server

--- src/main/java/org/example/greeting/server/GreetServiceImpl.java
+++ src/main/java/org/example/greeting/server/GreetServiceImpl.java
@@ -1,12 +1,10 @@
 package org.example.greeting.server;
 
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
-import com.proto.greet.GreetServiceGrpc;
-import com.proto.greet.Greeting;
+import com.proto.greet.*;
 import io.grpc.stub.StreamObserver;
 
 public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
   @Override
   public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
     // extract the fields we need
@@ -25,4 +23,28 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
     // complete the RPC call
     responseObserver.onCompleted();
   }
+
+  @Override
+  public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
+    String firstName = request.getGreeting().getFirstName();
+
+    try {
+      for (int i = 0; i < 10; i++) {
+        String result = "Hello " + firstName + ", response number: " + i;
+        GreetManyTimesResponse response = GreetManyTimesResponse
+            .newBuilder()
+            .setResult(result)
+            .build();
+        responseObserver.onNext(response);
+        Thread.sleep(1000L);
+
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      responseObserver.onCompleted();
+    }
+
+  }
+
 }
