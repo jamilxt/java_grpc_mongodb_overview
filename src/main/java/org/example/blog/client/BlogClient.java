9cf173b7c86742ef2c7b5fedfb98480b7c1b3a7d
jamilxt <jamilxt@gmail.com>
2021-02-06 17:54:07 +0600

    add: CreateBlog Client

--- src/main/java/org/example/blog/client/BlogClient.java
+++ src/main/java/org/example/blog/client/BlogClient.java
@@ -1,5 +1,12 @@
 package org.example.blog.client;
 
+import com.proto.blog.Blog;
+import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class BlogClient {
 
   public static void main(String[] args) {
@@ -11,6 +18,26 @@ public class BlogClient {
 
 
   private void run() {
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+        .usePlaintext()
+        .build();
+
+    BlogServiceGrpc.BlogServiceBlockingStub blogClient = BlogServiceGrpc.newBlockingStub(channel);
+
+    Blog blog = Blog.newBuilder()
+        .setAuthorId("jamilxt")
+        .setTitle("New blog!")
+        .setContent("Hello world this is my new blog!")
+        .build();
+
+    CreateBlogResponse createResponse = blogClient.createBlog(
+        CreateBlogRequest.newBuilder()
+            .setBlog(blog)
+            .build()
+    );
+
+    System.out.println("Received create blog response");
+    System.out.println(createResponse.toString());
 
   }
 
